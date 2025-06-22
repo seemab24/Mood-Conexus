40567e05b6f2dddb309966edb349a3cd28b7d152
Seemab Hassan <seemabhassan05@gmail.com>
2025-06-22 20:54:22 +0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// ✅ Use your repo name in base (case-sensitive)
 export default defineConfig({
-  base: '/Mood-Conexus/',
   plugins: [react()],
+  base: '/Mood-Conexus/'
 })
