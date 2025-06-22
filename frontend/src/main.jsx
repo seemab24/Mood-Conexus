6b5719c699c80e07359122d753188cd0e120267b
Seemab Hassan <seemabhassan05@gmail.com>
2025-06-22 22:21:14 +0500

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import store from './components/Redux/store.js';
-import { Provider } from 'react-redux';
+import store from './components/Redux/store.js'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom' // ✅ Add this
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <StrictMode>
+    <BrowserRouter basename="/Mood-Conexus/">  {/* ✅ Important */}
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </StrictMode>
 )
-
