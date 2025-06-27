4a92e78e72c4bbd812117a339103f41d5e37abe6
Seemab Hassan <seemabhassan05@gmail.com>
2025-06-28 01:27:20 +0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  base: '/Mood-Conexus/', // ✅ required for GitHub Pages!
   plugins: [react()],
-})
+    base: "/Mood-Conexus", 
+
+});
